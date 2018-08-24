cb923898427bd8caa6a3c6191a7c1c9b029164f4
Ben Wolters <18100784+woldinius@users.noreply.github.com>
2018-08-24 10:45:55 +0200

    Material der bisherigen Meetups

---
+++ archiv/2017-07-13 Guerilla Kanban/Scrumban - Gruppen- und Abteilungsmangement.mm
@@ -0,0 +1,20 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1455746065825" ID="ID_1432769893" MODIFIED="1455746080433" TEXT="Gruppen- und Abteilungsmangement">
+<node CREATED="1455746081824" ID="ID_102681721" MODIFIED="1455746094456" POSITION="right" TEXT="Mehrere Gruppen">
+<node CREATED="1455746197631" ID="ID_1329914083" MODIFIED="1455746200359" TEXT="Kanban">
+<node CREATED="1455746201509" ID="ID_1562448630" MODIFIED="1455746214204" TEXT="&#xdc;bergeordnetes Kanban Board"/>
+<node CREATED="1455746214491" ID="ID_448486518" MODIFIED="1455746235644" TEXT="Kaizen als Retrospektive"/>
+<node CREATED="1455746238002" ID="ID_913485671" MODIFIED="1455746256636" TEXT="Personal kann zwischen Scrum-Teams verschoben/gelihen werden"/>
+</node>
+</node>
+<node CREATED="1455746096833" ID="ID_881554353" MODIFIED="1455746107091" POSITION="right" TEXT="Projektteams">
+<node CREATED="1455746111928" ID="ID_774891560" MODIFIED="1455746138562" TEXT="Scrum eignet sich f&#xfc;r die einzelnen Teams"/>
+<node CREATED="1455746146945" ID="ID_6768484" MODIFIED="1455746163181" TEXT="Voraussetzungen ">
+<node CREATED="1455746164454" ID="ID_1729417666" MODIFIED="1455746173131" TEXT="5-9 Personen"/>
+<node CREATED="1455746173850" ID="ID_151898963" MODIFIED="1455746188793" TEXT="Sicherer Umgang mit Toolchains"/>
+<node CREATED="1455746189263" ID="ID_796055645" MODIFIED="1455746195025" TEXT="Extreme Programming"/>
+</node>
+</node>
+</node>
+</map>
